c76bef3d6205496a4d95cc4584daa227cde6eb5e
Bryan Ariana <arianabryan02@gmail.com>
2022-04-08 20:54:31 -0600

    Se agrega CRUD de empleados

---
+++ CRUDempleados/include/empleados.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#ifndef EMPLEADOS_H
+#define EMPLEADOS_H
+using namespace std;
+
+class empleados
+{
+    public:
+        void menu();
+		void insert();
+		void display();
+		void modify();
+		void search();
+		void delet();
+        empleados();
+        virtual ~empleados();
+
+    protected:
+    private:string id,nombre,telefono;
+};
+
+#endif // EMPLEADOS_H
---
+++ CRUDempleados/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "empleados.h"
+using namespace std;
+
+int main()
+{
+    empleados empleado;
+	empleado.menu();
+	return 0;
+}
